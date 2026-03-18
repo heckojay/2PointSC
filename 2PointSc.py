9e84c8476f5a5a0ee7c03911c4f9ae427242c4f0
Jericho Rosell <jerichorosell1@gmail.com>
2026-03-17 18:02:22 -0700

    Initial commit

---
+++ 2PointSc.py
@@ -0,0 +1,40 @@
+from pynput import mouse
+import mss
+import mss.tools
+
+#Getting Points
+points = []
+def on_click(x, y, button, pressed):
+    if pressed and button == mouse.Button.left:
+        points.append((x,y))
+        print(f"its here @ {(x,y)}")
+        if len(points) == 2:
+            return False # Stop listener
+    return None
+
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
+
+#Store Points
+(x1, y1), (x2,y2) = points
+
+left = min(x1, x2)
+top = min(y1, y2)
+width = abs(x2 - x1)
+height = abs(y2 - y1)
+
+#Screen Capture
+with mss.mss() as sct:
+    # The screen part to capture
+    if width == 0 or height == 0:
+        print("Invalid Region: Width or Height Is 0.")
+    else:
+        monitor = {"top": top, "left": left, "width": width, "height": height}
+        output = f"sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+        # Grab the data
+        sct_img = sct.grab(monitor)
+
+        # Save to the picture file
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+        print(output)
